36e59bb25f0c00f6737a9885c62aa4c7b4057fae
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-26 20:30:56 +0100

    Add song selection to root reducer

--- src/provider/reducers/index.ts
+++ src/provider/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import allPlaylistsReducer from '../../screens/AllPlaylists';
+import allPlaylistsReducer from '../../screens/AllPlaylists/reducer';
+import songSelectionReducer from '../../screens/SongSelection/reducer';
 
 const rootReducer = combineReducers({
   AllPlaylists: allPlaylistsReducer,
+  Songs: songSelectionReducer,
 });
 
 export default rootReducer;
