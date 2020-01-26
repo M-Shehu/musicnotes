5e3f4d373efd0e2d2965a524e391dd5b50b444a6
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-26 20:30:39 +0100

    Add initial state to All playlist reducer

--- src/screens/AllPlaylists/reducer.ts
+++ src/screens/AllPlaylists/reducer.ts
@@ -3,8 +3,15 @@
 */
 import { updatePlaylistWithSong } from './utils';
 import { UPDATE_PLAYLIST } from '../Playlist/constants';
+import { AllPlaylists } from '../../provider/store/stateInterfaces';
 
-function allPlaylistsReducer(state, action) {
+const initialState = <AllPlaylists>{
+  danceAll: { name: 'Dance all', songs: [] },
+  relaxation: { name: 'Relaxation', songs: [] },
+  roadTrip: { name: 'Road Trip', songs: [] },
+};
+
+function allPlaylistsReducer(state = initialState, action) {
   switch (action.type) {
     case UPDATE_PLAYLIST:
       return updatePlaylistWithSong(state, action);
