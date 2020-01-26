0e4dfb4ac5b27be35b7fa5a8de13ed42f9d44939
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-26 19:57:43 +0100

    Add initial state for Redux

---
+++ src/provider/store/initialState.ts
@@ -0,0 +1,10 @@
+const initialState = {
+  AllPlaylists: {
+    danceAll: { name: 'Dance all', songs: [] },
+    relaxation: { name: 'Relaxation', songs: [] },
+    roadTrip: { name: 'Road Trip', songs: [] },
+  },
+  Songs: ['Bohemian Rhapsody', 'Enter Sandman', 'Happy', 'Queen'],
+};
+
+export default initialState;
