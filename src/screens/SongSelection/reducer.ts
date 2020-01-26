db54a8c0368e4ebde8882de104f7f034b85e8811
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-26 20:30:24 +0100

    Add SongSelection reducer

---
+++ src/screens/SongSelection/reducer.ts
@@ -0,0 +1,16 @@
+/*
+    Song Selection reducer
+*/
+
+const initialState = <string[]>[
+  'Bohemian Rhapsody',
+  'Enter Sandman',
+  'Happy',
+  'Queen',
+];
+
+function songSelectionReducer(state = initialState, action) {
+  return state;
+}
+
+export default songSelectionReducer;
