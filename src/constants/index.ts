9812079b24f9732cc1e02baeb60134f589fb6568
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-27 22:56:27 +0100

    Update constants with colors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,12 @@
 export const ALL_PLAYLISTS_ROUTE = 'AllPlaylists';
 export const PLAYLIST_ROUTE = 'Playlist';
 export const SONG_SELECTION_ROUTE = 'SongSelection';
+
+//Colors
+export const Colors = {
+  primary: '#00A82D',
+  white: '#FFFFFF',
+  lightPrimary: '#F1FFE7',
+  darkPrimary: '6C464E',
+  black: '#000000',
+};
