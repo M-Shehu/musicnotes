115a402345994724c3f7241d8961d5193a12e492
M-Shehu <magnanimus.shehu@gmail.com>
2020-01-27 14:26:49 +0100

    Add new interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,22 @@
+import { UPDATE_PLAYLIST } from '../screens/Playlist/constants';
+
 export interface AllPlaylistsInterface {
   [prop: string]: { name: string; songs: string[]; color: string };
+}
+
+export interface StateInterface {
+  AllPlaylists: AllPlaylistsInterface;
+  Songs: string[];
+}
+
+export interface updatePlaylist {
+  key: string;
+  song: string;
+}
+
+export interface UpdatePlaylistAction {
+  type: typeof UPDATE_PLAYLIST;
+  payload: updatePlaylist;
 }
 
 export type NavigationOptionProps = {
