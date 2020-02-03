dc573eaac49c8a7f6ce146a80136c33bf73f6266
M-Shehu <magnanimus.shehu@gmail.com>
2020-02-03 01:14:27 +0100

    Update rootReducer to include current song

--- src/provider/reducers/index.ts
+++ src/provider/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import allPlaylistsReducer from '../../screens/AllPlaylists/reducer';
 import songSelectionReducer from '../../screens/SongSelection/reducer';
+import currentSongReducer from '../../screens/MusicPlayer/reducer';
 
 const rootReducer = combineReducers({
   AllPlaylists: allPlaylistsReducer,
   Songs: songSelectionReducer,
+  CurrentSong: currentSongReducer,
 });
 
 export default rootReducer;
