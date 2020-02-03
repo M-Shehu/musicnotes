b7d9751901f9ed980bf19a3662c8f6ba5e048054
M-Shehu <magnanimus.shehu@gmail.com>
2020-02-03 01:14:45 +0100

    Add current song interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,14 +1,22 @@
 import { UPDATE_PLAYLIST } from '../screens/Playlist/constants';
+import { UPDATE_CURRENT_SONG } from '../screens/MusicPlayer/constants';
 
 export interface AllPlaylistsInterface {
   // All playlist is saved in an object instead of an array for
   // instant lookup of playlists
-  [prop: string]: { name: string; songs: string[]; color: string };
+  [prop: string]: PlaylistInterface;
+}
+
+export interface PlaylistInterface {
+  name: string;
+  songs: SongsInterface[];
+  color: string;
 }
 
 export interface StateInterface {
   AllPlaylists: AllPlaylistsInterface;
-  Songs: string[];
+  Songs: SongsInterface[];
+  CurrentSong: CurrentSongInterface;
 }
 
 export interface updatePlaylist {
@@ -25,3 +33,17 @@ export type NavigationOptionProps = {
   headerTitle?: ((screenProps?) => any) | string;
   headerRight?: (screenProps?) => any;
 };
+export interface SongsInterface {
+  key: string;
+  songName: string;
+  artistName: string;
+}
+
+export interface CurrentSongInterface extends SongsInterface {
+  playlistKey: string;
+}
+
+export interface UpdateCurrentSongAction {
+  type: typeof UPDATE_CURRENT_SONG;
+  payload: CurrentSongInterface;
+}
